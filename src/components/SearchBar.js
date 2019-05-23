74645ee06512b5732c20a9522766ff3d1941b493
Daniel Nam <dannam83@gmail.com>
2019-05-23 11:22:03 -0400

    submission

---
+++ src/components/SearchBar.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import iconSearch from '../assets/images/iconSearch.svg';
+import { fetchMedia, searchMedia } from '../actions';
+import './SearchBar.css';
+
+class SearchBar extends Component {
+  state = { search: "" }
+
+  onChangeText = (event) => {
+    const { value } = event.target;
+    this.setState({ search: value });
+    this.props.searchMedia(value);
+  }
+
+  submitSearch = (event) => {
+    this.props.searchMedia(this.state.search);
+  }
+
+  render() {
+    return (
+      <div className="Search-bar-container">
+
+        <div className="Search-bar-div">
+          <input
+            className="Search-bar-input"
+            value={this.state.search}
+            type="text"
+            name="name"
+            placeholder='Search "Walmart"'
+            onChange={this.onChangeText}
+          />
+          <img src={iconSearch} className="Search-bar-icon" alt="logo" />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default connect(null, { fetchMedia, searchMedia })(SearchBar);
---
+++ src/components/ToggleDisplay.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Button from '@material-ui/core/Button';
+import windowSize from 'react-window-size';
+
+import './ToggleDisplay.css';
+import { displaySelect } from '../actions';
+
+class ToggleDisplay extends Component {
+  onClick(label) {
+    this.props.displaySelect(label);
+  }
+
+  render() {
+    const { display, windowWidth } = this.props;
+    const buttonSize = windowWidth > 500 ? "large" : "small";
+
+    const sliderButtonStyle = display === 'Slider' ?
+      selectedButtonStyle : unselectedButtonStyle;
+
+    const listButtonStyle = display === 'List' ?
+      selectedButtonStyle : unselectedButtonStyle;
+
+    return (
+      <div className="Toggle-display-container">
+        <div className="Toggle-display-div">
+          <label className="Toggle-display-label">Toggle Display</label>
+          <Button
+            style={sliderButtonStyle}
+            variant="contained"
+            size={buttonSize}
+            onClick={() => this.onClick('Slider')}
+          >
+            Slider
+          </Button>
+          <Button
+            style={listButtonStyle}
+            variant="contained"
+            size={buttonSize}
+            onClick={() => this.onClick('List')}
+          >
+            List
+          </Button>
+        </div>
+      </div>
+    )
+  }
+}
+
+const selectedButtonStyle = {
+  fontSize: '10px',
+  backgroundColor: 'rgba(0,0,131)',
+  color: 'white',
+  marginLeft: '9px',
+  width: '80px',
+}
+
+const unselectedButtonStyle = {
+  fontSize: '10px',
+  backgroundColor: 'white',
+  color: 'rgba(0,0,131)',
+  marginLeft: '9px',
+  width: '80px',
+}
+
+const mapStateToProps = (state) => {
+  const { display } = state;
+  return {
+    display
+  };
+};
+
+export default connect(mapStateToProps, { displaySelect })(windowSize(ToggleDisplay));
