011636ba78fad11ddaff1e74856c86b021730fbf
Daniel Nam <dannam83@gmail.com>
2019-05-25 00:11:37 -0400

    Updates

---
+++ precache-manifest.b780a9f0f9bf162deedf3541b5639e15.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aa009fd74e7e65acb821f02755eae1b",
+    "url": "/Carouselider/index.html"
+  },
+  {
+    "revision": "21ccf0f2e7b925e7455f",
+    "url": "/Carouselider/static/css/main.569a72d0.chunk.css"
+  },
+  {
+    "revision": "881323f1e58019420b50",
+    "url": "/Carouselider/static/js/2.0f1a52be.chunk.js"
+  },
+  {
+    "revision": "21ccf0f2e7b925e7455f",
+    "url": "/Carouselider/static/js/main.71877fe9.chunk.js"
+  },
+  {
+    "revision": "e04afac5a9eaecd1638f",
+    "url": "/Carouselider/static/js/runtime~main.e77fe861.js"
+  },
+  {
+    "revision": "a691ae182f83f3b74bf80e6049b661b9",
+    "url": "/Carouselider/static/media/BentonSansBook.a691ae18.otf"
+  },
+  {
+    "revision": "bc89cda3d934a75f3f2c470599779add",
+    "url": "/Carouselider/static/media/BentonSansCompressedBlack.bc89cda3.otf"
+  },
+  {
+    "revision": "7862606bbe2e444c981712fb47cc5ec5",
+    "url": "/Carouselider/static/media/BentonSansRegular.7862606b.otf"
+  },
+  {
+    "revision": "1b1f466cf575f7ba57ec989d0d0d8ecd",
+    "url": "/Carouselider/static/media/BentonSansWide.1b1f466c.otf"
+  },
+  {
+    "revision": "aa1853e4d327104861065d2f5cb960a0",
+    "url": "/Carouselider/static/media/BentonSansWideBlack.aa1853e4.otf"
+  }
+]);
